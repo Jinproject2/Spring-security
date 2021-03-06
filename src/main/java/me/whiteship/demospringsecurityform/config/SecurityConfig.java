b478e55989087603b5fee82944ad5d5390c7d451
CJ <cj>
2021-03-06 13:28:58 +0900

    3챕터 - logout 필터        > logout시 기본 로그인페이지로 이동 하는 경로를 루트 페이지로 변경 해본다.

--- src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -54,6 +54,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.formLogin();
         http.httpBasic();
 
+        http.logout().logoutSuccessUrl("/");
+
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
 }
