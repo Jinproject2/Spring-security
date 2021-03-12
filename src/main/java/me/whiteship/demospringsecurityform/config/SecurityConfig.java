1d3fc962a8a85af333a41aa3729c892df9160718
CJ <cj>
2021-03-12 22:53:08 +0900

    3챕터 - 인증/인가 필터 (ExceptionTranslationFilter)        > 권한(인증 실패) 시 기본 에러 문구가아니라 만든 페이지로 이동 시켜보기

--- src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
+++ src/main/java/me/whiteship/demospringsecurityform/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDecisionManager;
 import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.access.vote.AffirmativeBased;
@@ -14,10 +15,16 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.access.expression.WebExpressionVoter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -64,6 +71,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .changeSessionId()
                 .maximumSessions(1)
                     .maxSessionsPreventsLogin(false);
+
+        // TODO ExceptionTranslatorFilter -> FilterSecurityInterceptor (AccessDecisionManager, AffirmativeBased)
+        // TODO AuthenticationException -> AuthenticationEntryPoint
+        // TODO AccessDinedException -> AccessDeniedHandler
+
+        //http.exceptionHandling().accessDeniedPage("/access-denied");
+        http.exceptionHandling()
+            .accessDeniedHandler((httpServletRequest, httpServletResponse, e) -> {
+                UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                String username = principal.getUsername();
+                System.out.println(username + " is denied to access " + httpServletRequest.getRequestURI());
+                httpServletResponse.sendRedirect("/access-denied");
+            });
 
         SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
     }
